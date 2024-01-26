d05bbff7d64542df1dd412191fa00eba3a1e4046
Liam <clink.6@osu.edu>
2024-01-26 01:38:59 -0500

    Script to setup fake system

---
+++ make_fake_system.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FAKE_ROOT_DIR="$(pwd)/fake-root"
+
+mkdir -p $FAKE_ROOT_DIR
+
+sudo apt install debootstrap -y
+
+echo "installing ubuntu 18.04 into the fake root"
+sudo debootstrap bionic $FAKE_ROOT_DIR
+
+
+username="testuser"
+
+# Check if the user already exists within the chroot
+if sudo chroot "$FAKE_ROOT_DIR" id "$username" &>/dev/null; then
+    echo "User '$username' already exists inside the chroot."
+else
+    # Add the user within the chroot
+    sudo chroot "$FAKE_ROOT_DIR" useradd --create-home --shell /bin/bash "$username"
+    if sudo chroot "$FAKE_ROOT_DIR" id "$username" &>/dev/null; then
+        echo "User '$username' added successfully inside the chroot."
+    else
+        echo "Adding user '$username' inside the chroot failed."
+    fi
+fi
+
+# Ensure that the home folder is owned by the user
+sudo chroot $FAKE_ROOT_DIR chown --recursive "$username":"$username" /home/"$username"
+
+cp --recursive flycapture2-2.13.3.31-amd64 ./fake-root/home/"$username"/
